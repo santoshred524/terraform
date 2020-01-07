45e3733658139b54903537df8e9b6aa7b9078173
santoshred524 <46974840+santoshred524@users.noreply.github.com>
2020-01-06 20:27:44 -0600

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "access_key" {
+  description = "The access key for the AWS"
+  default   = ""
+}
+
+variable "secret_key" {
+  description = "The secret key for the AWS"
+  default   = ""
+}
+
+variable "region" {
+  description = "The Region of the project"
+  default   = ""
+}
+
+variable "project_name" {
+  description = "Project name"
+  default   = ""
+}
